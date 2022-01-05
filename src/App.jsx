e7e6ca12b65bdaf27c8f7c5ecf226941397d06b4
Nerval Gravie <asd@asd.com>
2022-01-05 20:44:48 +0800

    section 3 complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,58 @@
 import { useEffect, useMemo, useState } from "react";
 
 // import thirdweb
+import { ThirdwebSDK } from "@3rdweb/sdk";
+
 import { useWeb3 } from "@3rdweb/hooks";
 
+const sdk = new ThirdwebSDK("rinkeby");
+
+
+// We can grab a reference to our ERC-1155 contract.
+const bundleDropModule = sdk.getBundleDropModule(
+  "0x5DC5115973a23e569D02906B804Fe5AF9E22F9bd",
+);
+
 const App = () => {
-  // Use the connectWallet hook thirdweb gives us.
   const { connectWallet, address, error, provider } = useWeb3();
   console.log("👋 Address:", address)
 
-  // This is the case where the user hasn't connected their wallet
-  // to your web app. Let them call connectWallet.
+  // The signer is required to sign transactions on the blockchain.
+  // Without it we can only read data, not write.
+  const signer = provider ? provider.getSigner() : undefined;
+
+  const [hasClaimedNFT, setHasClaimedNFT] = useState(false);
+  // isClaiming lets us easily keep a loading state while the NFT is minting.
+  const [isClaiming, setIsClaiming] = useState(false);
+
+  // Another useEffect!
+  useEffect(() => {
+    // We pass the signer to the sdk, which enables us to interact with
+    // our deployed contract!
+    sdk.setProviderOrSigner(signer);
+  }, [signer]);
+
+  useEffect(() => {
+    if (!address) {
+      return;
+    }
+    return bundleDropModule
+      .balanceOf(address, "0")
+      .then((balance) => {
+        if (balance.gt(0)) {
+          setHasClaimedNFT(true);
+          console.log("🌟 this user has a membership NFT!")
+        } else {
+          setHasClaimedNFT(false);
+          console.log("😭 this user doesn't have a membership NFT.")
+        }
+      })
+      .catch((error) => {
+        setHasClaimedNFT(false);
+        console.error("failed to nft balance", error);
+      });
+  }, [address]);
+
   if (!address) {
     return (
       <div className="landing">
@@ -21,12 +64,67 @@ const App = () => {
     );
   }
   
-  // This is the case where we have the user's address
-  // which means they've connected their wallet to our site!
+  const mintNft = () => {
+    setIsClaiming(true);
+    // Call bundleDropModule.claim("0", 1) to mint nft to user's wallet.
+    bundleDropModule
+    .claim("0", 1)
+    .then(() => {
+      // Set claim state.
+      setHasClaimedNFT(true);
+      // Show user their fancy new NFT!
+      console.log(
+        `🌊 Successfully Minted! Check it our on OpenSea: https://testnets.opensea.io/assets/${bundleDropModule.address}/0`
+      );
+    })
+    .catch((err) => {
+      console.error("failed to claim", err);
+    })
+    .finally(() => {
+      // Stop loading state.
+      setIsClaiming(false);
+    });
+  }
+  
+  // Render mint nft screen.
   return (
-    <div className="landing">
-      <h1>👀 wallet connected, now what!</h1>
-    </div>);
+    <div className="mint-nft">
+      <h1>Mint your free 🍪DAO Membership NFT</h1>
+      <button
+        disabled={isClaiming}
+        onClick={() => mintNft()}
+      >
+        {isClaiming ? "Minting..." : "Mint your nft (FREE)"}
+      </button>
+    </div>
+  );
 };
+
+
+// const App = () => {
+//   // Use the connectWallet hook thirdweb gives us.
+//   const { connectWallet, address, error, provider } = useWeb3();
+//   console.log("👋 Address:", address)
+
+//   // This is the case where the user hasn't connected their wallet
+//   // to your web app. Let them call connectWallet.
+//   if (!address) {
+//     return (
+//       <div className="landing">
+//         <h1>Welcome to BoluDAO</h1>
+//         <button onClick={() => connectWallet("injected")} className="btn-hero">
+//           Connect your wallet
+//         </button>
+//       </div>
+//     );
+//   }
+  
+//   // This is the case where we have the user's address
+//   // which means they've connected their wallet to our site!
+//   return (
+//     <div className="landing">
+//       <h1>👀 wallet connected, now what!</h1>
+//     </div>);
+// };
 
 export default App;
